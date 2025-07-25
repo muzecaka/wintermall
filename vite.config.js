1b61ced8d24c11ca64a79f7b6fbc61bc45783c54
muzecaka <muzecaka@gmail.com>
2025-07-25 20:15:05 +0100

    fixed Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+    import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/wintermall/',
-});
+    export default defineConfig({
+      plugins: [react()],
+      base: '/wintermall/',
+      build: {
+        outDir: 'build',
+        rollupOptions: {
+          input: {
+            main: 'index.html', // Explicitly set index.html as entry
+          },
+        },
+      },
+    });
